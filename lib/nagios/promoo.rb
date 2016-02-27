888ae63a3fd46dba413dccc7929495ec2db2d9be
Boris Parak <parak@cesnet.cz>
2016-02-27 14:39:05 +0100

    Gem skeleton

---
+++ lib/nagios/promoo.rb
@@ -0,0 +1,7 @@
+require "nagios/promoo/version"
+
+module Nagios
+  module Promoo
+    # Your code goes here...
+  end
+end
---
+++ lib/nagios/promoo/version.rb
@@ -0,0 +1,5 @@
+module Nagios
+  module Promoo
+    VERSION = "0.0.1"
+  end
+end
